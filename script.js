9a93888bf9e4edc41bd0a28be9467ebce4f23f0d
Elvis Michael <elvismichael888@gmail.com>
2024-08-24 20:51:09 +0100

    feat: add reset functionality
    The reset button feature is added to allow for
    possibility of prompting user to dictate number
    of squares per side.

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
+// Set counter for box creation
 let boxCounter = 0;
+let actualValue;
+
 
 // Write a function that creates the grid
 function boxCreation() {
     // CREATE THE DIVS
     let newDiv = document.createElement('div');
 
-    // Add class and ID
+    // Add class
     newDiv.className = 'newBox';
-    newDiv.id = 'newPoint';
 
     // Add attribute
     newDiv.setAttribute('title', 'My New Point');
 
     //Set properties
     newDiv.style.display = 'flex';
-    newDiv.style.height = '60px';
-    newDiv.style.width = '60px';
+    newDiv.style.height = '10px';
+    newDiv.style.width = '10px';
     newDiv.style.border = '2px solid black';
     newDiv.style.backgroundColor = 'white';
 
     //APPEND TO THE CONTAINER
     let container = document.querySelector(".container");
-
     container.append(newDiv);
 
     // ADD HOVER EFFECT
@@ -36,15 +37,35 @@ function boxCreation() {
 
 }
 
+function reset(e) {   
+    console.log('EVENT TYPE: '+e.type);
+    const userInput = prompt('Number of squares per side(<100):');
+    console.log(userInput);
 
+    let intValue = Number(userInput);
+    if (intValue < 100) {
+        actualValue = intValue ** 2;
+        console.log(actualValue);
 
-// Write a loop that creates multiple boxes
-while (boxCounter < 256) {
-    console.log(boxCounter);
-    boxCreation();
-    boxCounter++;
+        // Clear previous grid
+        document.querySelector(".container").innerHTML = "";
+
+        // Reset box counter
+        boxCounter = 0;
+
+        // Write a loop that creates boxes
+        while (boxCounter < actualValue) {
+            console.log(boxCounter);
+            boxCreation();
+            boxCounter++;
+        }
+    }
+    return;
+    
 }
 
+// Get the button element 
+const button = document.querySelector('button');
 
-
-
+// Add an event listener to the button for sending prompt message
+button.addEventListener('click', reset);
